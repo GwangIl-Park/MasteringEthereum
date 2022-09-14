ce6eefe33db379e2d25f042a0e179fa8e7d5d02b
GwangIl-Park <rkasud0@gmail.com>
2022-09-14 17:42:05 +0900

    Add overunderflow, unexpectedEther

---
+++ 09/delegateCall/contracts/FibonacciBalance.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.7.3;
+
+contract FibonacciBalance {
+    address public fibonacciLibrary;
+    uint public calculatedFibNumber;
+    uint public start = 3;
+    uint public withdrawalCounter;
+}
---
+++ 09/overUnderflow/contracts/TimeLock.sol
@@ -0,0 +1,23 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.7.3;
+
+contract TimeLock {
+    mapping (address => uint) public balances;
+    mapping (address => uint) public lockTime;
+    
+    function deposit() public payable {
+        balances[msg.sender] += msg.value;
+        lockTime[msg.sender] = block.timestamp + 1 weeks;
+    }
+
+    function increaseLockTime(uint _secondsToIncrease) public {
+        lockTime[msg.sender] += _secondsToIncrease;
+    }
+
+    function withdraw() public {
+        require(balances[msg.sender]>0);
+        require(block.timestamp > lockTime[msg.sender]);
+        balances[msg.sender] = 0;
+        msg.sender.transfer(balances[msg.sender]);
+    }
+}
